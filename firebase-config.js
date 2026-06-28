b3217942401decd709f354bce9530498736b3201
apkcode1-lgtm <apkcode1@gmail.com>
2026-06-28 17:59:10 +0300

    Remove Firebase configuration and initialization code

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,1 @@
-// True 
-const firebaseConfig = {
-  apiKey: "AIzaSyBgXU6N4cMV2q-d3XeFzvgFT98gJ1GM7Ws",
-  authDomain: "tirfe-app.firebaseapp.com",
-  databaseURL: "https://tirfe-app-default-rtdb.firebaseio.com",
-  projectId: "tirfe-app",
-  storageBucket: "tirfe-app.firebasestorage.app",
-  messagingSenderId: "228622358915",
-  appId: "1:228622358915:web:c9ff3039a6d6cf66613eb6"
-};
-
-// ፋየርቤዝ ከዚህ በፊት initialize ካልተደረገ ብቻ እንዲጀምር ማድረግ
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
-const db = firebase.database();
 
